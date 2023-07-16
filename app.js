e5baf93a21ebe0a965aee5075e51cf3b9a2928aa
Jonathan <jonathankendrick697@gmail.com>
2023-07-16 11:39:59 -0700

    Re-working my implementation
    re-coding the project again because the previous version was not clean code.

--- app.js
+++ app.js
@@ -1,32 +1,3 @@
-let btn=document.querySelector('button');
-const container=document.querySelector('.container');
-let condition=false;
-
-
-/* default dimension */
-    function makeRows(rows, cols) {
-        container.style.setProperty('--grid-rows', rows);
-        container.style.setProperty('--grid-cols', cols);
-
-        for(let i=0;i<rows*cols;i++){
-            const square=document.createElement('div');
-            container.appendChild(square)
-            square.classList.add('box');
-        }};
-        makeRows(16, 16);
-    
-    
-    
-    btn.addEventListener('click',function(){
-       
-        let userInt=parseInt(prompt('dimesions?'));
-        container.innerHTML=" "
-        if(userInt>100)userInt=100;//if dimension is greater than 100 dimension is equal to 100
-        makeRows(userInt, userInt);//make row and col of inputed inputed dimension
-     
-        });
-
-
 
 
 
