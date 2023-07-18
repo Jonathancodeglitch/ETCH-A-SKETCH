3c02dabdb7d1f3ea4da11ecfec39cf725e267d2c
Jonathan <jonathankendrick697@gmail.com>
2023-07-18 14:37:03 -0700

    Add rgb color functionality

--- app.js
+++ app.js
@@ -46,14 +46,56 @@ function displayUserDimension() {
 dimennsionBtn.addEventListener('click', displayUserDimension);
 
 //when user moves on boxes
-let isDrawing=false;
-Grid.addEventListener('mousedown',()=> isDrawing=true);
-Grid.addEventListener('mouseover', draw);
-Grid.addEventListener('mouseup',()=> isDrawing=false);
+let isDrawing = false;
+let rainbow = false;
+
+Grid.addEventListener('mousedown', (e) => {
+  e.preventDefault();
+  isDrawing = true;
+});
+
+Grid.addEventListener('mouseup', (e) => {
+  e.preventDefault();
+  isDrawing = false;
+});
+
 //change the color of the box to black
-function draw(e) {
+Grid.addEventListener('mouseover', (e) => {
   if (isDrawing && e.target.classList.contains('box')) {
-    let box = e.target;
+    e.preventDefault();
+    draw(e);
+  }
+});
+
+Grid.addEventListener('click', (e) => {
+  if (e.target.classList.contains('box')) {
+    draw(e);
+  }
+});
+
+function draw(e) {
+  let box = e.target;
+  if (rainbow) {
+    box.style.backgroundColor = getRainbowColour();
+  } else {
     box.style.backgroundColor = 'black';
   }
+}
+
+//when rainbow is clicked change pen ink to diffrent color
+//when eraser is click change pen to eraser
+//when pan is click it show default back to pen
+//when clear btn is press eraser everything in the gread
+
+let rainbowBtn = document.getElementById('rainbow');
+
+rainbowBtn.addEventListener('click', () => {
+  rainbow = true;
+});
+
+function getRainbowColour() {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+  return `rgb(${red},${green},${blue})`;
 }
